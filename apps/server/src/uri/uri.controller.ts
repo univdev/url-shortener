fc43b07ef380b078030e71928f798385b5d29eb4
Chanyoung Park <rhdwkrxla68@gmail.com>
2025-02-09 17:35:20 +0900

    feat: Get generated short urls

--- apps/server/src/uri/uri.controller.ts
+++ apps/server/src/uri/uri.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiBody } from '@nestjs/swagger';
 import { UriService } from './uri.service';
-import { IgnoreResultInterceptor } from 'src/ignore-result-interceptor/ignore-result-interceptor.decorator';
 import { ConfigService } from '@nestjs/config';
 
 @Controller('uri')
@@ -10,6 +9,16 @@ export class UriController {
     private readonly uriService: UriService,
     private readonly configService: ConfigService,
   ) {}
+
+  @Get('/')
+  public async getUris(
+    @Query('offset') offset: number,
+    @Query('limit') limit: number,
+  ) {
+    const numOffset = Number(offset);
+    const numLimit = Number(limit);
+    return this.uriService.getUris(numOffset, numLimit);
+  }
 
   @Post('/')
   @ApiBody({
@@ -30,7 +39,6 @@ export class UriController {
   }
 
   @Get('/qr')
-  @IgnoreResultInterceptor()
   public async getQR(@Query('shortKey') shortKey: string) {
     const uri = await this.uriService.getFullUrlByShortKey(shortKey);
     const qr = await this.uriService.getQR(
